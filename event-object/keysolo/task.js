d4c93b72fac21f0ef4ab0c99301513e58a96a009
Alexey Shishkin <shayu78@mail.ru>
2020-08-01 01:27:19 +0400

    Решение задания лекции №3 (соло на клавиатуре)

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -1,13 +1,17 @@
+'use strict';
+
 class Game {
   constructor(container) {
     this.container = container;
     this.wordElement = container.querySelector('.word');
     this.winsElement = container.querySelector('.status__wins');
     this.lossElement = container.querySelector('.status__loss');
+    this.timerElement = container.querySelector('.status__timer');
 
     this.reset();
 
     this.registerEvents();
+    let idTimer = null;
   }
 
   reset() {
@@ -24,6 +28,27 @@ class Game {
       В случае правильного ввода слова вызываем this.success()
       При неправильном вводе символа - this.fail();
      */
+    this.activateTimer();
+
+    document.addEventListener('keyup', (event) => {
+      switch (event.key) {
+        case 'Alt':
+        case 'Shift':
+        case 'Control': {
+          break;
+        }
+        default: event.key.toLowerCase() === this.currentSymbol.textContent.toLowerCase() ? this.success() : this.fail();
+      }
+    });
+  }
+
+  activateTimer() {
+    if (this.idTimer) clearInterval(this.idTimer);
+    let timerValue = this.timerElement.textContent;
+    this.idTimer = setInterval(() => {
+      this.timerElement.textContent = --timerValue;
+      if (timerValue < 0) this.fail();
+    }, 1000);
   }
 
   success() {
@@ -38,36 +63,39 @@ class Game {
       this.reset();
     }
     this.setNewWord();
+    this.activateTimer();
   }
 
   fail() {
-    if (++this.lossElement.textContent === 5) {
+    if (++this.lossElement.textContent === 3) {
       alert('Вы проиграли!');
       this.reset();
     }
     this.setNewWord();
+    this.activateTimer();
   }
 
   setNewWord() {
     const word = this.getWord();
-
     this.renderWord(word);
+    this.timerElement.textContent = word.length;
   }
 
   getWord() {
     const words = [
-        'bob',
-        'awesome',
-        'netology',
-        'hello',
-        'kitty',
-        'rock',
-        'youtube',
-        'popcorn',
-        'cinema',
-        'love',
-        'javascript'
-      ],
+      'bob',
+      'awesome',
+      'netology',
+      'hello',
+      'kitty',
+      'rock',
+      'youtube',
+      'popcorn',
+      'cinema',
+      'love',
+      'javascript',
+      'Я люблю kitkat'
+    ],
       index = Math.floor(Math.random() * words.length);
 
     return words[index];
@@ -77,7 +105,7 @@ class Game {
     const html = [...word]
       .map(
         (s, i) =>
-          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
+          `<span class="symbol ${i === 0 ? 'symbol_current' : ''}">${s}</span>`
       )
       .join('');
     this.wordElement.innerHTML = html;
@@ -87,4 +115,3 @@ class Game {
 }
 
 new Game(document.getElementById('game'))
-
