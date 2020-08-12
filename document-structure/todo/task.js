f49bf54f53eb188f35cedb1e84b4524875f2a25c
Alexey Shishkin <shayu78@mail.ru>
2020-08-12 07:14:20 +0400

    Доработка решения задания лекции №6 (простой список дел): - оптимизация локального хранилища - очистка полей формы - запрет обновления страницы по кнопке формы

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,13 +1,20 @@
 'use strict';
 
 const tasksList = document.querySelector('.tasks__list');
-tasksList.innerHTML = localStorage.getItem('tasksKey');
+
+let tasksLSArray = JSON.parse(localStorage.getItem("tasksKey"));
+tasksLSArray ? tasksList.innerHTML = tasksLSArray.map((value) => getTaskMarkup(value)).join('') : tasksLSArray = [];
+
 // устанавливаем обработчики на удаление заданий, восстановленных из хранилища
 [...document.querySelectorAll('.task__remove')].forEach((value) => {
     value.addEventListener("click", (event) => removeTask(event.currentTarget));
 });
 
-document.querySelector('.tasks__add').addEventListener("click", () => addTask(taskInput.value));
+document.querySelector('.tasks__add').addEventListener("click", (event) => {
+    event.preventDefault();
+    addTask(taskInput.value);
+});
+
 const taskInput = document.querySelector('.tasks__input');
 taskInput.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
@@ -17,20 +24,28 @@ taskInput.addEventListener("keypress", (event) => {
 });
 
 function addTask(text) {
+    if (tasksLSArray.includes(text)) return;
+    taskInput.value = '';
     if (text.trim()) {
-        tasksList.insertAdjacentHTML('beforeEnd',
-            `<div class="task">
-            <div class="task__title">
-                ${text}
-            </div>
-            <a href="#" class="task__remove">&times;</a>
-        </div>`);
-        localStorage.setItem('tasksKey', tasksList.innerHTML);
+        tasksList.insertAdjacentHTML('beforeEnd', getTaskMarkup(text));
+        tasksLSArray.push(text);
+        localStorage.setItem('tasksKey', JSON.stringify(tasksLSArray));
         tasksList.lastChild.querySelector('.task__remove').addEventListener("click", (event) => removeTask(event.currentTarget));
     }
 }
 
 function removeTask(element) {
-    element.closest('.task').remove();
-    localStorage.setItem('tasksKey', tasksList.innerHTML);
+    const taskElement = element.closest('.task');
+    tasksLSArray = tasksLSArray.filter((value) => value !== taskElement.querySelector('.task__title').textContent.trim());
+    localStorage.setItem('tasksKey', JSON.stringify(tasksLSArray));
+    taskElement.remove();
+}
+
+function getTaskMarkup(text) {
+    return `<div class="task">
+    <div class="task__title">
+    ${text}
+    </div>
+    <a href="#" class="task__remove">&times;</a>
+    </div>`;
 }
