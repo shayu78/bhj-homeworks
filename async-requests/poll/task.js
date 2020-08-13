2f4ef1fec4687e71221ea92198831019af83e1be
Alexey Shishkin <shayu78@mail.ru>
2020-08-13 07:02:51 +0400

    Решение задания лекции №7 (опрос с выбором результатов)

---
+++ async-requests/poll/task.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const answersList = document.querySelector('.poll__answers');
+
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "https://netology-slow-rest.herokuapp.com/poll.php");
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === xhr.DONE) {
+    if (xhr.status >= 200 && xhr.status < 400) {
+      const valuteInfo = JSON.parse(xhr.responseText);
+      document.querySelector('.poll__title').textContent = valuteInfo.data.title;
+      answersList.innerHTML = valuteInfo.data.answers.reduce((dataHTML, value) => dataHTML + getAnswerMarkup(value), '');
+      document.querySelector('.poll__answers').addEventListener('click', (event) => {
+        if (event.target.classList.contains('poll__answer')) {
+          alert("Спасибо, ваш голос засчитан!");
+          const xhr = new XMLHttpRequest();
+          xhr.open("POST", "https://netology-slow-rest.herokuapp.com/poll.php");
+          xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+          xhr.onreadystatechange = function () {
+            if (xhr.readyState === xhr.DONE) {
+              if (xhr.status >= 200 && xhr.status < 400) {
+                const valuteInfo = JSON.parse(xhr.responseText);
+                const totalVotes = valuteInfo.stat.reduce((total, value) => total + value.votes, 0);
+                answersList.innerHTML = valuteInfo.stat.reduce((dataHTML, value) => {
+                  return dataHTML + getAnswerStatMarkup({ "answer": value.answer, "votes_percent": value.votes * 100 / totalVotes });
+                }, '');
+              } else console.error(`Ошибка ${xhr.status}`);
+            }
+          };
+          xhr.send(`vote=${valuteInfo.id}&answer=${valuteInfo.data.answers.findIndex((item) => item === event.target.textContent.trim())}`);
+        }
+      });
+    } else console.error(`Ошибка ${xhr.status}`);
+  }
+};
+xhr.send();
+
+function getAnswerMarkup(text) {
+  return `<button class="poll__answer">
+    ${text}
+  </button>`;
+}
+
+function getAnswerStatMarkup(data) {
+  return `<p>
+    ${data.answer}: ${Number.parseFloat(data.votes_percent).toFixed(2)}%
+  </p>`;
+}
