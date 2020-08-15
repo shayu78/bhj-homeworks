3d44bb44c9d56be885eaeaaa151c992fa90094f1
Alexey Shishkin <shayu78@mail.ru>
2020-08-15 14:59:15 +0400

    Доработка решения задания лекции №7 (анимация загрузки данных): добавлен try..catch при операции парсинга

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,8 +1,15 @@
 "use strict";
 
 const itemsList = document.querySelector('#items');
-let dataLSArray = JSON.parse(localStorage.getItem("valuteInfoKey"));
-dataLSArray ? itemsList.innerHTML = dataLSArray.reduce((dataHTML, value) => dataHTML + getValuteMarkup(value), '') : dataLSArray = [];
+
+let dataLSArray;
+try {
+    dataLSArray = JSON.parse(localStorage.getItem("valuteInfoKey"));
+    dataLSArray ? itemsList.innerHTML = dataLSArray.reduce((dataHTML, value) => dataHTML + getValuteMarkup(value), '') : dataLSArray = [];
+} catch (exception) {
+    console.log(`Некорректный формат данных - ${exception.name} ${exception.message}`);
+    dataLSArray = [];
+}
 
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "https://netology-slow-rest.herokuapp.com");
@@ -10,19 +17,24 @@ xhr.onreadystatechange = function () {
     if (xhr.readyState === xhr.DONE) {
         if (xhr.status >= 200 && xhr.status < 400) {
             document.querySelector('.loader').classList.remove('loader_active');
-            const valuteInfo = JSON.parse(xhr.responseText);
-            const dataArray = Object.entries(valuteInfo.response.Valute);
-            let dataHTML = '';
-            dataLSArray = dataArray.map((value) => {
-                const data = {
-                    charCode: value[1].CharCode,
-                    value: value[1].Value
-                };
-                dataHTML += getValuteMarkup(data);
-                return data;
-            });
-            localStorage.setItem('valuteInfoKey', JSON.stringify(dataLSArray));
-            itemsList.innerHTML = dataHTML;
+            try {
+                const valuteInfo = JSON.parse(xhr.responseText);
+                const dataArray = Object.entries(valuteInfo.response.Valute);
+                let dataHTML = '';
+                dataLSArray = dataArray.map((value) => {
+                    const data = {
+                        charCode: value[1].CharCode,
+                        value: value[1].Value
+                    };
+                    dataHTML += getValuteMarkup(data);
+                    return data;
+                });
+                localStorage.setItem('valuteInfoKey', JSON.stringify(dataLSArray));
+                itemsList.innerHTML = dataHTML;
+            } catch (exception) {
+                console.log(`Некорректный формат данных - ${exception.name} ${exception.message}`);
+                return;
+            }
         } else console.error(`Ошибка ${xhr.status}`);
     }
 };
