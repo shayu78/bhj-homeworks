b2c80312df9839d1336911ad1b3636d131e806b2
Alexey Shishkin <shayu78@mail.ru>
2020-08-16 08:15:56 +0400

    Доработка решения задачи лекции №8 (текстовый редактор): убрана кнопка сохранения, сохранение информации автоматически перед обновлением страницы

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -5,37 +5,20 @@ textareaElement.value = localStorage.getItem('textEditor');
 textareaElement.style.marginBottom = "10px";
 textareaElement.style.display = "block";
 
-const saveButtonElement = document.createElement("button");
 const clearButtonElement = document.createElement("button");
-const saveTextElement = document.createTextNode('Сохранить содержимое');
 const clearTextElement = document.createTextNode('Очистить содержимое');
-saveButtonElement.appendChild(saveTextElement);
 clearButtonElement.appendChild(clearTextElement);
 
-saveButtonElement.dataset.operation = 'save';
-clearButtonElement.dataset.operation = 'clear';
-
-textareaElement.insertAdjacentElement('afterend', saveButtonElement);
-saveButtonElement.insertAdjacentElement('afterend', clearButtonElement);
+textareaElement.insertAdjacentElement('afterend', clearButtonElement);
 
 document.querySelectorAll('button').forEach((value) => {
     value.style.margin = "0 10px 10px 0";
     value.style.padding = "5px";
 });
 
-document.addEventListener('click', (event) => {
-    if (event.target.tagName.toLowerCase() === 'button') {
-        switch (event.target.dataset.operation) {
-            case 'save': {
-                localStorage.setItem('textEditor', textareaElement.value);
-                break;
-            }
-            case 'clear': {
-                textareaElement.value = '';
-                localStorage.removeItem('textEditor');
-                break;
-            }
-            default:
-        }
-    }
+clearButtonElement.addEventListener('click', (event) => {
+    textareaElement.value = '';
+    localStorage.removeItem('textEditor');
 });
+
+window.addEventListener('beforeunload', (event) => localStorage.setItem('textEditor', textareaElement.value));
